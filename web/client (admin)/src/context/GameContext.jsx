0b019bc60f6a208109dc6ef79bf401955e0b6d41
alvitom <muhammadalvito2@gmail.com>
2024-07-12 21:39:34 +0700

    Commit

--- web/client (admin)/src/context/GameContext.jsx
+++ web/client (admin)/src/context/GameContext.jsx
@@ -20,7 +20,7 @@ export const GameProvider = ({ children }) => {
   const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "multipart/form-data",
     },
   });
 
