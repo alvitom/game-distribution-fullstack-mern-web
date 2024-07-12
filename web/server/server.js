8b71d0953fd7a7674fa23e6c1b2891a68719ae9c
alvitom <muhammadalvito2@gmail.com>
2024-07-12 19:24:36 +0700

    Commit

--- web/server/server.js
+++ web/server/server.js
@@ -26,15 +26,10 @@ const midtransRouter = require("./routes/midtrans");
 app.use(morgan("combined"));
 app.use(
   cors({
-    origin: [process.env.ADMIN_BASE_URL_DEV, process.env.ADMIN_BASE_URL_STG, process.env.STORE_BASE_URL_DEV, process.env.STORE_BASE_URL_STG],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
